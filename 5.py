05051841363f772f07ba5154b87ea0401d2b4659
Vitor <84788940+VBSX@users.noreply.github.com>
2023-02-02 11:54:05 -0300

    update replaceWowels

--- 5.py
+++ 5.py
@@ -11,4 +11,28 @@
 
 
 def replaceWowels(string):
-    
+    vowels = ['a','e','i','o','u']
+    loop = True
+    output_string = ''
+    for char_of_string in string:
+        
+        print(char_of_string,'\n')
+        for vow in vowels:
+            # print('vowl ', vow)
+            if char_of_string == vow:
+                output_string += '?'
+                loop = False
+                # print(loop)
+                break
+        print(output_string)
+        if loop:    
+            output_string +=char_of_string
+        loop = True
+                
+            
+                
+    return output_string
+
+
+string = input('Coloque uma palavra: ')
+print(replaceWowels(string))
